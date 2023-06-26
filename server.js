87ac9256807a2e7359f6ee82dfdfa43408e866a7
dashonaholt <dashona.n.holt@gmail.com>
2023-06-26 15:55:31 -0500

    updates

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
-const express = require("express")
-const app = express()
-const morgan = require("morgan")
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
 
 //Middleware (for every request)
-app.use(express.json()) //looks for a request body, and turns it into 'req.body'
-app.use(morgan('dev'))
+app.use(express.json()); //looks for a request body, and turns it into 'req.body'
+app.use(morgan("dev"));
 
 //Routes (go here)
-app.use("/stretches", require("./routes/stretchRouter.js"))
+app.use("/stretches", require("./routes/stretchRouter.js"));
 
 //connect to MongoDB
-mongoose.connect("mongodb+srv://dashonanholt:8SFEA9XdeBpXb5MD@cluster0.gd7ezzi.mongodb.net/stretchdb",) 
-    .then (() => console.log("connected to the DB"))
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to the DB");
+    // Start the Express server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the DB:", error);
+  });
 
-// Error handler 
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+// Error handler
 app.use((err, req, res, next) => {
-    console.log(err)
-    return res.send({errMsg: err.message})
-})
-
+  console.log(err);
+  return res.send({ errMsg: err.message });
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 //Server Listen
 app.listen(9000, () => {
-console.log("The server is running on Port 9000")
-})
+  console.log("The server is running on Port 9000");
+});
